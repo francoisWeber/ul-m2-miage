8fdf6b478bb05802a65a8ca1cd6decd9c4f5ea44
François Weber <francois.weber.commercial@gmx.fr>
2025-01-05 19:31:26 +0100

    Better user creation

--- dockerfiles/jupyter-hub/user_init.sh
+++ dockerfiles/jupyter-hub/user_init.sh
@@ -1,27 +1,95 @@
 #!/bin/bash
-USERNAME=iaas
+# # # Setup users and sharing
+
+# Main (useless) user
+ADMIN_USER=iaas
 PASSWORD=azerty
-useradd -u 1000 -g 1000 -m --shell /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | chpasswd
+useradd -u 1000 -g 1000 -m --shell /bin/bash "$ADMIN_USER"
+echo "$ADMIN_USER:$PASSWORD" | chpasswd
 
-# Set generic password
-PASSWORD="azerty"
+# # # Other configs # # # #
 
-# Path to the file containing the usernames
+# Shared config
+SHARED_FOLDER="/workshop"
+SHARED_GROUP="workshop_group"
+
+DATA_FOLDER="/datasets"
+
+# Users list
 USER_FILE="./users.txt"
+
+# User passwords
+SALT="sel"
+PASSWORD_BACKUP_DIR="/home/iaas/passwords.txt"
+
+
+# # # # Functions # # # # 
+# password generation
+generate_password() {
+  local username="$1"
+  # Generate password: first 8 chars of the salted SHA256 of the username
+  echo -n "$SALT $username" | sha256sum | awk '{print substr($1,1,8)}'
+}
+
+
+# # # # SCRIPT # # # # 
+
+# # GROUP CREATION  # #
+# Create the shared group if it doesn't exist
+if ! getent group "$SHARED_GROUP" > /dev/null; then
+  groupadd "$SHARED_GROUP"
+  echo "Group $SHARED_GROUP created."
+else
+  echo "Group $SHARED_GROUP already exists."
+fi
+
+# # SHARED FOLDER
+# Create the shared folder if it doesn't exist
+if [ ! -d "$SHARED_FOLDER" ]; then
+  mkdir -p "$SHARED_FOLDER"
+  echo "Shared folder $SHARED_FOLDER created."
+else
+  echo "Shared folder $SHARED_FOLDER already exists."
+fi
+
+# # USERS  # #
 
 # Loop through each line in the file
 while IFS= read -r USERNAME
 do
   # Check if username is non-empty
   if [ -n "$USERNAME" ]; then
-    # Create the user with home directory (-m) and default shell (/bin/bash)
-    useradd -m --shell /bin/bash "$USERNAME"
+
+    if id "$USERNAME" &>/dev/null; then
+      echo "User $USERNAME already exists."
+    else
+      # Create the user with home directory (-m) and default shell (/bin/bash)
+      useradd -m --shell /bin/bash "$USERNAME"
+    fi
+
+    password=$(generate_password "$USERNAME")
     
     # Set the user's password to 'azerty'
-    echo "$USERNAME:$PASSWORD" | chpasswd
+    echo "$USERNAME:$password" | chpasswd
     
-    echo "Created user $USERNAME with password $PASSWORD"
+    echo "Created user $USERNAME with password $password"
+    echo "$USERNAME - $password" >> $PASSWORD_BACKUP_DIR
+
+    # Add user to workshop group
+    usermod -a -G "$SHARED_GROUP" "$USERNAME"
+
+    # simlink the workshop folder 
+    ln -sf "$SHARED_FOLDER" "/home/$USERNAME/workshop"
   fi
 done < "$USER_FILE"
 
+mv $PASSWORD_BACKUP_DIR /home/francois/passwords.txt
+chown francois /home/francois/passwords.txt
+
+# # SHARED FOLDER CONFIG # # 
+ADMIN_USER=francois
+
+# Set ownership and permissions for the shared folder
+chown $ADMIN_USER:$SHARED_GROUP "$SHARED_FOLDER"
+
+chmod 770 "$SHARED_FOLDER"
